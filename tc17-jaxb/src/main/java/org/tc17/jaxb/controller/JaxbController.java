59b9bb5a7e663de45216fe66eaf6dfdccf7ee27c
roman <roman.mishchenko@gmail.com>
2013-01-01 20:32:14 +0100

    marshall/unmarshall #1

--- tc17-jaxb/src/main/java/org/tc17/jaxb/controller/JaxbController.java
+++ tc17-jaxb/src/main/java/org/tc17/jaxb/controller/JaxbController.java
@@ -1,5 +1,14 @@
 package org.tc17.jaxb.controller;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.tasclin1.mopet.domain.Drug;
+import org.tasclin1.mopet.domain.Task;
 import org.tasclin1.mopet.domain.Tree;
 import org.tasclin1.mopet.service.MopetService;
 import org.tc17.jaxb.core.Appx;
@@ -23,6 +34,7 @@ import org.tc17.jaxb.core.Patientx;
 import org.tc17.jaxb.core.Pvariablex;
 import org.tc17.jaxb.core.TaskPatientx;
 import org.tc17.jaxb.core.TaskRegimex;
+import org.tc17.jaxb.core.Taskx;
 import org.tc17.jaxb.core.Timesx;
 import org.tc17.jaxb.core.Treex;
 
@@ -62,32 +74,124 @@ public class JaxbController {
 	return "Read from XML: " + drugXml;
     }
 
+    @RequestMapping(value = "/xxml={htmlId}", method = RequestMethod.GET,
+    		produces = "application/xml")
+    public @ResponseBody
+    Treex xxml(@PathVariable
+    String htmlId, Model model) {
+    	log.debug(1);
+    	log.debug(htmlId);
+    	String pasteId = htmlId;
+    	log.debug("-------------" + pasteId);
+    	String pasteNodeId = pasteId.split("_")[1];
+    	int pasteNodeId2 = Integer.parseInt(pasteNodeId);
+    	// String readJaxb = readJaxb(pasteId);
+    	log.debug("-------------" + pasteNodeId2);
+    	Treex taskx = loadTaskx(pasteNodeId2);
+//    	Drugx drugx = loadDrugx(pasteNodeId2);
+//    	Tree drugT = buildTree(drugx);
+//    	log.debug("-------------" + drugT);
+    	return taskx;
+    }
+    public Tree buildTree(TaskRegimex taskX) {
+    	Tree taskT = new Tree();
+    	taskT.setTabName("task");
+    	taskT.setMtlO(new Task());
+    	taskT.getTaskO().setTask(taskX.getTaskName());
+    	taskT.getTaskO().setTaskvar(taskX.getTaskvar());
+    	taskT.getTaskO().setType(taskX.getType());
+    	if(taskX.getDrug().size()>0){
+    		taskT.setChildTs(new ArrayList<Tree>());
+    		for (Drugx drugX : taskX.getDrug()) {
+    			Tree buildTree = buildTree(drugX);
+    			taskT.getChildTs().add(buildTree);
+    		}
+    	}
+    	return taskT;
+    }
+    public Tree buildTree(Drugx drugX) {
+    	Tree drugT = new Tree();
+    	drugT.setTabName("drug");
+    	drugT.setMtlO(new Drug());
+    	drugT.getDrugO().setDrug(drugX.getDrug());
+    	drugT.getDrugO().setGeneric(new Drug());
+    	drugT.getDrugO().getGeneric().setDrug(drugX.getGeneric());
+    	ArrayList<Tree> taskTchilds = new ArrayList<Tree>();
+    	Dosex dose = drugX.getDose();
+    	if(null!=dose)
+    	{
+    		
+    	}
+    	return drugT;
+    }
+    private TaskRegimex loadTaskx(Integer pasteId) {
+    	TaskRegimex taskX = null;
+    	JAXBContext newInstance;
+    	URL url;
+    	log.debug(1);
+    	try {
+    		log.debug(2);
+    		newInstance = JAXBContext.newInstance(TaskRegimex.class);
+    		log.debug(3);
+    		Unmarshaller createUnmarshaller = newInstance.createUnmarshaller();
+    		log.debug(4);
+    		String urlStr = "http://localhost:8080/tc17-web/xml=x_" + pasteId;
+    		log.debug(urlStr);
+			url = new URL(urlStr);
+			log.debug(url);
+    		taskX = (TaskRegimex) createUnmarshaller.unmarshal(url);
+    		log.debug(taskX);
+    	} catch (MalformedURLException e) {
+    		e.printStackTrace();
+    	} catch (JAXBException e) {
+    		e.printStackTrace();
+    	}
+    	return taskX;
+    }
+    private Drugx loadDrugx(Integer pasteId) {
+    	Drugx drugx = null;
+    	JAXBContext newInstance;
+    	URL url;
+    	try {
+    		newInstance = JAXBContext.newInstance(Drugx.class);
+    		Unmarshaller createUnmarshaller = newInstance.createUnmarshaller();
+    		url = new URL("http://localhost:8080/tc17-web/xml=x_" + pasteId);
+    		drugx = (Drugx) createUnmarshaller.unmarshal(url);
+    		log.debug(drugx);
+    	} catch (MalformedURLException e) {
+    		e.printStackTrace();
+    	} catch (JAXBException e) {
+    		e.printStackTrace();
+    	}
+    	return drugx;
+    }
     @RequestMapping(value = "/xml={htmlId}", method = RequestMethod.GET,
     		produces = "application/xml")
     public @ResponseBody
     Treex xml(@PathVariable
-    String htmlId, Model model) {
-	log.debug(1);
-	Integer id = mopetService.getIdFromHtmlId(htmlId);
-	log.debug(id);
-	Tree t0 = mopetService.readNodes4(id, model);
-	Treex mtlX = null;
-	if (t0.isTask()) {
-	    mtlX = regimeTaskx(t0);
-	} else if (t0.isDrug()) {
-	    mtlX = regimeDrugx(t0);
-	} else if (t0.isPatient()) {
-	    mtlX = regimePatientx(t0);
-	} else if (t0.isDose()) {
-	    mtlX = new Dosex(t0);
-	} else if (t0.isDay()) {
-	    mtlX = regimeDrugDay(t0);
-	} else if (t0.isTimes()) {
-	    mtlX = new Timesx(t0);
-	} else {
-	    log.info("TODO!");
-	}
-	return mtlX;
+    		String htmlId, Model model) {
+    	log.debug(123);
+    	log.debug(1);
+    	Integer id = mopetService.getIdFromHtmlId(htmlId);
+    	log.debug(id);
+    	Tree t0 = mopetService.readNodes4(id, model);
+    	Treex mtlX = null;
+    	if (t0.isTask()) {
+    		mtlX = regimeTaskx(t0);
+    	} else if (t0.isDrug()) {
+    		mtlX = regimeDrugx(t0);
+    	} else if (t0.isPatient()) {
+    		mtlX = regimePatientx(t0);
+    	} else if (t0.isDose()) {
+    		mtlX = new Dosex(t0);
+    	} else if (t0.isDay()) {
+    		mtlX = regimeDrugDay(t0);
+    	} else if (t0.isTimes()) {
+    		mtlX = new Timesx(t0);
+    	} else {
+    		log.info("TODO! \n"+t0);
+    	}
+    	return mtlX;
     }
 
     private Patientx regimePatientx(Tree taskT) {
